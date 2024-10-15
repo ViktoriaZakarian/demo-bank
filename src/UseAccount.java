351588f30ed51a00f2b5ac0f23d44bc9b90cd5bc
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-15 18:44:25 +0300

    "refactor  useaccount class"

--- src/UseAccount.java
+++ src/UseAccount.java
@@ -2,20 +2,26 @@ public class UseAccount {
     public static void main(String[] args) {
         Account myAccount = new Account();
         Account yourAccount = new Account();
+
         myAccount.name = "Барри";
         myAccount.address = "222 Inner Lane";
         myAccount.balance = 24.02;
+
         yourAccount.name = "Джейн";
         yourAccount.address = "121 Outer Street";
         yourAccount.balance = 55.63;
         myAccount.display();
         System.out.print(" плюс $");
+
         System.out.print(myAccount.getInterest(5.00));
+
         System.out.println(" дохода");
         yourAccount.display();
         double yourInterestRate = 7.00;
         System.out.print(" плюс $");
+
         double yourInterestAmount = yourAccount.getInterest(yourInterestRate);
+
         System.out.print(yourInterestAmount);
         System.out.println(" дохода");
     }
